469dd05afbaf45a4ce5dc40a23d191414363aad3
Damin <daminhaidar08@gmail.com>
2023-05-31 16:12:52 +0200

    for google colab

---
+++ runfalcon.py
@@ -0,0 +1,30 @@
+from transformers import AutoTokenizer, AutoModelForCausalLM
+import transformers
+import torch
+
+model = "tiiuae/falcon-7b"
+
+tokenizer = AutoTokenizer.from_pretrained(model)
+pipeline = transformers.pipeline(
+    "text-generation",
+    model=model,
+    tokenizer=tokenizer,
+    torch_dtype=torch.bfloat16,
+    trust_remote_code=True,
+    device_map="auto",
+)
+while True:
+    query = input("\nEnter a query: ")
+    if query == "exit":
+        break
+
+    sequences = pipeline(
+    query,
+    max_length=200,
+    do_sample=True,
+    top_k=10,
+    num_return_sequences=1,
+    eos_token_id=tokenizer.eos_token_id,
+    )
+    for seq in sequences:
+        print(f"Result: {seq['generated_text']}")
